e3bad833d2f3bf510ce745d4eec72ec71ff1c56a
Tommy Mann <thomas.mann@mymail.champlain.edu>
2018-11-10 17:37:07 -0500

    deleted everything in repo

--- GameAI/component steering SDL/MathTools.cpp
+++
@@ -1,9 +0,0 @@
-#include "MathTools.h"
-
-Vector2D getDirectionAsVector(float direction)
-{
-	float x = cos(direction);
-	float y = sin(direction);
-
-	return Vector2D(x, y);
-}
--- GameAI/component steering SDL/MathTools.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "Vector2D.h"
-
-Vector2D getDirectionAsVector(float direction);
